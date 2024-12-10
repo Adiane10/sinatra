72d69d8aa9b591d723d6160812637c970b50cfda
Adiane10 <adianecerqueiraleite@gmail.com>
2024-12-10 07:00:52 -0300

    Delete test directory

--- test/integration/simple_app.rb
+++
@@ -1,6 +0,0 @@
-require "bundler/setup"
-require "sinatra"
-
-get "/" do
-  "OK"
-end
--- test/integration/zeitwerk_app.rb
+++
@@ -1,8 +0,0 @@
-require "bundler/setup"
-# This needs to come first so that sinatra require goes through zeitwerk loader
-require "zeitwerk"
-require "sinatra"
-
-get "/" do
-  "OK"
-end
